e3ea12442af009a0a986cef5c7cf40a5e04923d5
Miaofei Wang (from Dev Box) <miaofeiw@microsoft.com>
2024-09-30 16:27:12 +0800

    Updates

---
+++ functions.js
@@ -0,0 +1,2 @@
+!function(){"use strict";CustomFunctions.associate("TESTSTRINGENUM",(function(t,n){return n})),CustomFunctions.associate("TESTSTRINGENUM2",(function(t,n){return t+n})),CustomFunctions.associate("TESTNUMBERENUM",(function(t,n){return n.reduce(((t,n)=>t+n),0)}))}();
+//# sourceMappingURL=functions.js.map
